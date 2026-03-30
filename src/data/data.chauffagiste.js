a588d54f659763144051391141db2297a5908f12
mrflemmardmcpc-gif <mrflemmard.mcpc@gmail.com>
2026-03-30 19:24:57 +0200

    Export module Chauffagiste [auto]

--- src/data/data.chauffagiste.js
+++ src/data/data.chauffagiste.js
@@ -1,6 +1,29 @@
 export default {
   "value": {
-    "sections": [],
-    "categories": []
+    "sections": [
+      {
+        "id": 1774891481039,
+        "name": "tes",
+        "color": "#6d4aff",
+        "emoji": "📁"
+      }
+    ],
+    "categories": [
+      {
+        "id": 1774891487540,
+        "name": "test",
+        "icon": "📦",
+        "sectionId": 1774891481039,
+        "color": "#f59e42",
+        "subs": [
+          {
+            "id": 1,
+            "title": "Nouveau module",
+            "text": "<p>zdkfjbz</p>",
+            "color": "#10b981"
+          }
+        ]
+      }
+    ]
   }
 };
