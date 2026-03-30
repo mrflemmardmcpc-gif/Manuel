cbcabe277a6c8511a8b3ec7131e3eda504f7c875
mrflemmardmcpc-gif <mrflemmard.mcpc@gmail.com>
2026-03-30 19:46:33 +0200

    Export module modulesconfig [auto]

--- src/data/data.modulesconfig.js
+++ src/data/data.modulesconfig.js
@@ -1,17 +1,4 @@
 export default {
-  "Plombier": { requiresAuth: false },
-  "Chauffagiste": { requiresAuth: false },
-  "Maçon": { requiresAuth: true },
-  "Couvreur": { requiresAuth: true },
-  "Menuisier": { requiresAuth: true },
-  "Électricien": { requiresAuth: true },
-  "Peintre": { requiresAuth: true },
-  "Carreleur": { requiresAuth: true },
-  "Plâtrier": { requiresAuth: true },
-  "Étancheur": { requiresAuth: true },
-  "Charpentier": { requiresAuth: true },
-  "Dessinateur": { requiresAuth: true },
-  "Serrurier": { requiresAuth: true },
-  "Ferrailleur": { requiresAuth: true },
-  "Calorifugeur": { requiresAuth: true }
+  "test": true,
+  "ts": 1774892793046
 };
